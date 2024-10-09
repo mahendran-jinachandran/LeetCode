d96290511b52f220298d4a857862f8164e6ba14d
mahendran29 <68937987+mahendran29@users.noreply.github.com>
2024-10-09 23:06:19 +0530

    Time: 29 ms (6.38%), Space: 44.8 MB (5.39%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    String addZeroPrefix(String number, int numberOfZeroes) {
+        String newNumber = number;
+        for(int i=0; i < numberOfZeroes; i++) {
+            newNumber = "0" + newNumber;
+        }
+
+        return newNumber;
+    }
+
+    public String addBinary(String a, String b) {
+        
+        int aLength = a.length();
+        int bLength = b.length();
+
+        if(aLength < bLength) {
+            a = addZeroPrefix(a, bLength - aLength);
+        } else if (aLength > bLength) {
+            b = addZeroPrefix(b, aLength - bLength);
+        }
+
+        System.out.println("A: " +a);
+        System.out.println("B: " +b);
+        int remainder = 0;
+        String finalString = "";
+        for(int i = a.length() - 1; i >= 0; i--) {
+            int aNumber = a.charAt(i) - '0';
+            int bNumber = b.charAt(i) - '0';
+
+            int total = aNumber + bNumber + remainder;
+            if(total % 2 != 0 && (aNumber != 1 || bNumber != 1)) {
+                remainder = 0;
+            } else if (total % 2 == 0 && (aNumber == 0 && bNumber == 0)) {
+                remainder = 0;
+            } else {
+                remainder = 1;
+            }
+
+            finalString = Integer.toString(total%2) + finalString;
+            System.out.println("ITER: "+finalString);
+        }
+
+        if(remainder == 1 && !finalString.isEmpty()) {
+            finalString = "1" + finalString;
+        }
+
+        return finalString;
+    }
+}
