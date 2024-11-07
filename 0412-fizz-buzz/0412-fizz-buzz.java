45e99b18c4c37f5222490c0253f70b793e683d06
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2024-11-07 23:44:01 +0530

    Time: 1 ms (99.64%), Space: 45.3 MB (19.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -10,7 +10,7 @@ class Solution {
             } else if (i % 5 == 0) {
                 list.add("Buzz");
             } else {
-                list.add(String.valueOf(i));
+                list.add(Integer.toString(i));
             }
        } 
 
